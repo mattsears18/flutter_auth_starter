388b19be85009f2f824c6ce148ffcc5a29d02be6
mattsears18 <mattsears18@gmail.com>
2019-08-04 18:50:43 -0400

    link to local packages

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# [0.1.0] - 04 August 2019
+
+- firebase_auth 0.14.0 compatible
--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_auth_base/flutter_auth_base.dart';
+import 'package:flutter_auth_firebase/flutter_auth_firebase.dart';
 import 'core/auth/mock/mock_service.dart';
 
 import 'core/imageProviders/combined_image_provider.dart';
@@ -12,6 +13,20 @@ AuthService createMockedAuthService() {
   authService.postAuthPhotoProvider = new CombinedPhotoProvider()
     ..add(new AuthUserImageProvider(service: authService))
     ..add(new GravatarProvider(missingImageType: ImageType.MysteryMan));
+
+  return authService;
+}
+
+AuthService createFirebaseAuthService() {
+  var authService = new FirebaseAuthService();
+  authService.authProviders.addAll([
+    new FirebaseEmailProvider(service: authService),
+    new FirebaseGoogleProvider(service: authService)
+  ]);
+  authService.linkProviders.addAll([
+    new FirebaseEmailProvider(service: authService),
+    new FirebaseGoogleProvider(service: authService)
+  ]);
 
   return authService;
 }
